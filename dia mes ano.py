7b2ced764154e6107af8c41ef9b30c7d8e9df12a
vtpaim <164569739+vtpaim@users.noreply.github.com>
2024-04-09 18:52:58 -0300

    Add files via upload

---
+++ dia mes ano.py
@@ -0,0 +1,4 @@
+dia= input('Qual o dia que você nasceu?')
+mes= input('Qual o mês que você nasceu?')
+ano = input('Qual o ano que você nasceu?')
+print('Você nasceu dia', dia,'de', mes , 'de', ano)
---
+++ soma de dois numeros.py
@@ -0,0 +1,4 @@
+num1= input('Diga um número ')
+num2= input('Diga outro número ')
+soma= int(num1) + int(num2)
+print('A soma dos dois números é',soma)
